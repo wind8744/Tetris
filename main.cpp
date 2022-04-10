cb604b7a561f3e66fd11bd32f7c55d3c531dbd89
hyuna <wind8744@naver.com>
2022-04-10 10:48:18 +0900

    test 1

--- main.cpp
+++ main.cpp
@@ -6,33 +6,34 @@ using namespace std;
 
 int main()
 {
-	CTetris* bg = new CTetris;
-	bg->SetBlock();
-	int tick = 0;
+	// 1. test ~ !! 10 : 46
+	//CTetris* bg = new CTetris;
+	//bg->SetBlock();
+	//int tick = 0;
 
-	while (1)
-	{
-		if (GetAsyncKeyState(VK_UP) & 0x8001)
-			bg->Spin();
+	//while (1)
+	//{
+	//	if (GetAsyncKeyState(VK_UP) & 0x8001)
+	//		bg->Spin();
 
-		if (GetAsyncKeyState(VK_LEFT) & 0x8001)
-			bg->MoveBlock(2);
+	//	if (GetAsyncKeyState(VK_LEFT) & 0x8001)
+	//		bg->MoveBlock(2);
 
-		if (GetAsyncKeyState(VK_RIGHT) & 0x8001)
-			bg->MoveBlock(3);
+	//	if (GetAsyncKeyState(VK_RIGHT) & 0x8001)
+	//		bg->MoveBlock(3);
 
-		if (GetAsyncKeyState(VK_DOWN) & 0x8001)
-			bg->MoveBlock(1);
+	//	if (GetAsyncKeyState(VK_DOWN) & 0x8001)
+	//		bg->MoveBlock(1);
 
-		tick++;
-		if (tick % 10 == 0)
-			bg->MoveBlock(1);
+	//	tick++;
+	//	if (tick % 10 == 0)
+	//		bg->MoveBlock(1);
 
-		bg->PrintMap();
-		Sleep(100);
-	}
+	//	bg->PrintMap();
+	//	Sleep(100);
+	//}
 
-	delete(bg);
+	//delete(bg);
 
 	return 0;
 }
